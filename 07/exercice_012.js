8ebf72748d10bf201e8069efb980cc3ad7660745
Felipe Roiko Felix da Silva <roiko2010@gmail.com>
2024-11-02 18:09:35 -0300

    Condições
    Condições em JS
    -Simples (if)
    -Composta (if - else - if else)
    -Multipla (Swtich)

---
+++ 07/exercice_012.js
@@ -0,0 +1,11 @@
+
+var idade = 64
+
+console.log(`Você tem ${idade} anos.`)
+if (idade >= 18 && idade <=18){
+    console.log('Vota, obrigatório!')
+}else if(idade >= 16 || idade >= 65){
+        console.log('Voto opcional!')
+    } else {
+        console.log('Não vota!')
+}
